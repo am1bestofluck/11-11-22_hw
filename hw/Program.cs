e8bc1ee1b76eb6e3cc552812fda5e470281e09a4
am_6733 <anton6733@gmail.com>
2022-11-16 00:44:21 +0200

    akkerman

--- hw/Program.cs
+++ hw/Program.cs
@@ -2,7 +2,7 @@
 // Console.WriteLine("Hello, World!");
 using static System.Console;
 Main();
-int ValidateInput(string iInvite, bool iPositive=true)
+int ValidateInput(string iInvite, bool iPositive=true,bool iIncludeZero=false)
 {
     WriteLine(iInvite);
     bool isValid=false;
@@ -11,17 +11,37 @@ int ValidateInput(string iInvite, bool iPositive=true)
     {
         string bufferStr=ReadLine()!;
         isValid=Int32.TryParse(bufferStr, out output);
-        if (!isValid)
+        if (!isValid)//совсем уж явный мусор
             {
                 WriteLine("Ещё хочу :)");
-            }        
-        if (iPositive)
-        {
-            isValid=output > 0 ? true: false;
-            if (output<0)
-            {
-                WriteLine("Только положительные");
+                continue;
             }
+        if (!iIncludeZero)//оглашаем запрет ноля
+        {
+            if (output==0 && isValid)
+            {
+                WriteLine("В этот раз ноль запрещён.");
+            }
+        }        
+        if (iPositive)//проверка на допустимость: могут быть запрещены отрицательные с или без нуля
+        {
+            if(!iIncludeZero)
+            {
+                isValid=output > 0 ? true: false;
+                if (output<0)
+                {
+                    WriteLine("Только положительные");
+                }
+            }
+            else
+            {
+                isValid=output >= 0 ? true: false;
+                if (output<0)
+                {
+                    WriteLine("Только положительные");
+                }
+            }
+            
         }
     }
     return output;
@@ -45,18 +65,27 @@ string NumberLIneRecursion(int iBase,int iIndex=0)
     else return $"{NumberLIneRecursion(iBase,++iIndex)} {iIndex}";
 }
 
-int GetSumOfPositiveRange(int numberStart, int numberEnd)
+int GetSumOfPositiveRange(int iNumberStart, int iNumberEnd)
 {
 
-    if (numberStart==numberEnd+1) return 0;//включительно наверно оО; все же знают что сумма чисел от 1 до 10 это 55 а не 45
-    else return numberStart+GetSumOfPositiveRange(++numberStart,numberEnd);
+    if (iNumberStart==iNumberEnd+1) return 0;//включительно наверно оО; все же знают что сумма чисел от 1 до 10 это 55 а не 45
+    else return iNumberStart+GetSumOfPositiveRange(++iNumberStart,iNumberEnd);
 }
 
-ulong AckermannFunction(int m, int n)
+ulong AckermannFunction(ulong m, ulong n)//это функция ПОТОМ начала быть штангой для вычислений. А ДО этого что она делала?
 {
-    if (m>0 && n>0) return 0;
-    else if (m==0 %% n>0) return 1;
-    else if 
+    if (m==0)
+    {
+        return n+1;
+    }
+    else if (m>0 && n==0)
+    {
+        return AckermannFunction(m-1,1);
+    }
+    else 
+    {
+        return AckermannFunction(m-1,AckermannFunction(m,n-1));
+    }
 }
 void Main()
 {
@@ -75,21 +104,24 @@ void Main()
     void T2()
     {
         int[] valuesT2= new int[2];
-        valuesT2[0]=ValidateInput("Откуда считаем?", iPositive:false);
-        valuesT2[1]=ValidateInput("До какого числа считаем?", iPositive:false);
+        valuesT2[0]=ValidateInput("Откуда считаем?", iPositive:false, iIncludeZero:true);
+        valuesT2[1]=ValidateInput("До какого числа считаем?", iPositive:false, iIncludeZero:true);
         valuesT2[0]=valuesT2[0]>0? valuesT2[0]:0;
         valuesT2[1]=valuesT2[1]>0? valuesT2[1]:0;//сказано явно, сумма натуральных; больше диагностики = меньше головомойки!
-        WriteLine($"Сумма натуральных чисел: {GetSumOfPositiveRange(numberStart:valuesT2.Min(),numberEnd:valuesT2.Max())}");
+        int output= valuesT2[0]==valuesT2[0]? 0: GetSumOfPositiveRange(iNumberStart:valuesT2.Min(),iNumberEnd:valuesT2.Max());//чтобы 3 и 3 не давали на выходе 3
+        WriteLine($"Сумма натуральных чисел: {output}");
     }
     Break();
     WriteLine(external_todo[28]);//t3 15
     T3();
     void T3()
     {
+        WriteLine("В текущей реализации на 5,0 уже вылетает.");
         int[] valuesT2= new int[2];
-        valuesT2[0]=ValidateInput("m?");
-        valuesT2[1]=ValidateInput("n?");
-        WriteLine(String.Join(" ",valuesT2));
+        valuesT2[0]=ValidateInput("m?",iIncludeZero:true);
+        valuesT2[1]=ValidateInput("n?",iIncludeZero:true);
+        WriteLine($@"A({valuesT2[0]},{valuesT2[1]}) = {AckermannFunction(
+            m:Convert.ToUInt64(valuesT2[0]),
+            n:Convert.ToUInt64(valuesT2[1]))}");
     }
-    Break();
 }
